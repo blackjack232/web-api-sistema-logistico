155bcbed10ae036e9e3e67e433f9fb1f0df7eb21
Fredy Alexander España Garcia <faespanag@uqvirtual.edu.co>
2025-05-19 13:17:52 -0500

    Implementacion de sokets y redis

--- src/app/seguimiento-envios/page.tsx
+++ src/app/seguimiento-envios/page.tsx
@@ -4,6 +4,7 @@ import { buscarEnvioPorGuia } from "@/api/envios";
 import { useAuth } from "@/auth/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 interface Envio {
   numero_guia: string;
@@ -13,15 +14,17 @@ interface Envio {
   apellido_Destinatario: string;
   estado: string;
 }
-
+const socket = io("http://localhost:3002");
 export default function SeguimientoEnvios() {
   const [busqueda, setBusqueda] = useState("");
   const [loading, setLoading] = useState(false);
   const [resultados, setResultados] = useState<Envio | null>(null);
   const [token, setToken] = useState("");
-  const { isAuthenticated , isLoading} = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
   const router = useRouter();
-
+  const [estados, setEstados] = useState<
+    { numeroGuia: string; nuevoEstado: string }[]
+  >([]);
 
   const buscarPorGuia = async () => {
     setLoading(true);
@@ -29,7 +32,7 @@ export default function SeguimientoEnvios() {
       const response = await buscarEnvioPorGuia(busqueda, token);
       if (response.esExitoso) {
         setResultados(response.data.data);
-        console.log("response.data.data");
+        console.log(estados);
       } else {
         setResultados(null);
       }
@@ -40,7 +43,6 @@ export default function SeguimientoEnvios() {
       setLoading(false);
     }
   };
-
 
   const getEstadoColor = (estado: string) => {
     switch (estado.toLowerCase()) {
@@ -54,17 +56,42 @@ export default function SeguimientoEnvios() {
         return "bg-gray-400";
     }
   };
- useEffect(() => {
-  if (isLoading) return; 
+  useEffect(() => {
+    if (isLoading) return;
 
-  if (!isAuthenticated) {
-    router.replace("/login");
-  } else {
-    const storedToken = localStorage.getItem("authToken");
-    if (storedToken) setToken(storedToken);
-  }
-}, [isAuthenticated, isLoading, router]);
+    if (!isAuthenticated) {
+      router.replace("/login");
+    } else {
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) setToken(storedToken);
+    }
+  }, [isAuthenticated, isLoading, router]);
 
+  useEffect(() => {
+    socket.on("envio_actualizado", (data) => {
+      const info = JSON.parse(data);
+      setEstados((prevEstados) => [
+        ...prevEstados,
+        {
+          numeroGuia: info.numeroGuia,
+          nuevoEstado: info.nuevoEstado,
+        },
+      ]);
+      setResultados((prev) => {
+        if (prev && prev.numero_guia === info.numeroGuia) {
+          return {
+            ...prev,
+            estado: info.nuevoEstado,
+          };
+        }
+        return prev;
+      });
+    });
+
+    return () => {
+      socket.off("envio_actualizado");
+    };
+  }, []);
 
   if (!isAuthenticated) return null;
   return (
@@ -82,42 +109,40 @@ export default function SeguimientoEnvios() {
           onChange={(e) => setBusqueda(e.target.value)}
         />
         <button
-           type="button"
-            disabled={loading}
+          type="button"
+          disabled={loading}
           className={`min-w-[120px] flex justify-center items-center gap-2 bg-orange-700 text-white font-bold py-2 px-4 rounded-md transition ${
-              loading
-                ? "bg-orange-500 cursor-not-allowed"
-                : "hover:bg-orange-800"
-            }`}
+            loading ? "bg-orange-500 cursor-not-allowed" : "hover:bg-orange-800"
+          }`}
           onClick={buscarPorGuia}
         >
           {loading ? (
-              <>
-                <svg
-                  className="animate-spin h-5 w-5 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v8z"
-                  ></path>
-                </svg>
-                Iniciando...
-              </>
-            ) : (
-              "Buscar"
-            )}
+            <>
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8z"
+                ></path>
+              </svg>
+              Iniciando...
+            </>
+          ) : (
+            "Buscar"
+          )}
         </button>
       </div>
       <table className="w-full table-auto border-collapse">
@@ -133,13 +158,8 @@ export default function SeguimientoEnvios() {
           {resultados ? (
             <tr className="text-center border-t">
               <td className="p-2 border">{resultados.numero_guia}</td>
-              <td className="p-2 border">
-                {resultados.cedula_remitente}
-              </td>
-              <td className="p-2 border">
-                {resultados.cedula_destinatario}{" "}
-
-              </td>
+              <td className="p-2 border">{resultados.cedula_remitente}</td>
+              <td className="p-2 border">{resultados.cedula_destinatario} </td>
               <td className="p-2 border">
                 <span
                   className={`text-white px-2 py-1 rounded ${getEstadoColor(
@@ -159,6 +179,24 @@ export default function SeguimientoEnvios() {
           )}
         </tbody>
       </table>
+      <div className="text-gray-500 text-center mt-24">
+        <h2>📦 Estados de Envío</h2>
+        {estados.length === 0 ? (
+          <p>No hay actualizaciones aún.</p>
+        ) : (
+          <ul>
+            {estados.map((estado, index) => (
+              <li key={index}>
+                <strong>
+                  {" "}
+                  {index + 1} : {estado.nuevoEstado}
+                </strong>
+                :
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
